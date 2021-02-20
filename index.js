28e8cd47b61d668da70b724e47012edd4d7d58b3
fang.yang <yangfang2028@126.com>
2021-02-20 12:58:05 +0800

    📝 js 中的宏任务与微任务

--- index.js
+++ index.js
@@ -21,3 +21,23 @@ console.log(object.propertyIsEnumerable('a'));
 
 console.log(new Date); 
 console.log(Date())
+
+
+// setTimeout 是浏览器api,产生宏任务
+var r = new Promise(function(resolve, reject){
+    console.log("a");
+    resolve()
+});
+setTimeout(()=>console.log("d"), 0)
+r.then(() => console.log("c"));
+console.log("b")
+
+
+// function sleep(duration) {
+//     return new Promise(function(resolve, reject) {
+//         console.log("b");
+//         setTimeout(resolve,duration);
+//     })
+// }
+// console.log("a");
+// sleep(5000).then(()=>console.log("c"));
